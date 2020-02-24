62b59df6f09d737dd7c28df91b8a246e4e274bce
Danilo <danribeiro.u2@gmail.com>
2020-02-24 15:26:26 -0300

    commmit com bug

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,15 @@
 const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
 const router = express.Router();
 
-const  rootDir = require('../util/path');
-
-router.get('/', (req, res, next)=>{
-    res.sendFile(path.join(rootDir,'views', 'shop.html'));
+router.get('/', (req, res, next) => {
+    const products = adminData.products;
+    res.render('shop', { prods: products, docTitle: 'Shop' });
 });
 
 module.exports = router;
