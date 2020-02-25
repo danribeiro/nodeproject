7833b4d002d3525d99197f48a82d3f5827ae1802
danribeiro <danribeiro.u2@gmail.com>
2020-02-25 16:49:56 -0300

    capitulo 6 encerrado

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -16,9 +16,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
-
+   
 app.use((req, res, next) => {
-    res.status(404).render('404');
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -10,11 +10,12 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.render('add-product', { pageTitle: 'Add product' });
+  res.render('add-product', { pageTitle: 'Add product' , path:'/admin/add-product'});
 });
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
+  console.log(req.body);
   products.push({ title: req.body.title });
   res.redirect('/');
 });
